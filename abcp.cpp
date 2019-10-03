38bf86e6200b04e5dbd85e0428b875080c2ca2c5
fbk <frederico.klein@todo.todo>
2019-10-03 16:54:31 +0100

    updated code to latest library, added alignment option

--- abcp.cpp
+++ abcp.cpp
@@ -24,19 +24,34 @@ double askstuff(string, double);
 
 int main(int argc, char * argv[]) try
 {
+    //I will acquire same dimensions for depth and colour
+    int img_width = 1280;
+    int img_height = 720;
 
     // Create a simple OpenGL window for rendering:
-    window app(1280, 720, "Automatic Brassica-pics Capture ");
+    window app(2*img_width, img_height, "Automatic Brassica-pics Capture ");
     // Declare two textures on the GPU, one for color and one for depth
     texture depth_image, color_image;
 
     // Declare depth colorizer for pretty visualization of depth data
     rs2::colorizer color_map;
 
+
+    //configures streams resolution
+    rs2::config cfg;
+    cfg.enable_stream(RS2_STREAM_COLOR, -1, img_width, img_height, rs2_format::RS2_FORMAT_RGB8, 0);
+    cfg.enable_stream(RS2_STREAM_DEPTH, -1, img_width, img_height, rs2_format::RS2_FORMAT_ANY, 0);
+
     // Declare RealSense pipeline, encapsulating the actual device and sensors
     rs2::pipeline pipe;
     // Start streaming with default recommended configuration
-    pipe.start();
+    //pipe.start();
+    //starts pipeline with new configuration
+    pipe.start(cfg);
+
+    //align?
+    rs2::align align_to_depth(RS2_STREAM_DEPTH);
+    rs2::align align_to_color(RS2_STREAM_COLOR);
 
     int i = 0;
     int numofpics = 10;
@@ -47,6 +62,9 @@ int main(int argc, char * argv[]) try
     numofpics = askstuff("How many pics do you want",10);
 
     sizeofhead = askstuff("What's the size of this head",2.7);
+
+    std::string additional_stuff;
+    additional_stuff = askstuff("Anything else to say about the pic?", "");
 
     vector<int> compression_params;
     compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
@@ -68,13 +86,29 @@ int main(int argc, char * argv[]) try
     outfile << "picsof:" + pictype << std::endl;
     outfile << "numpics:" + std::to_string(numofpics) << std::endl;
     outfile << "sizeofhead:" + std::to_string(sizeofhead) << std::endl;
+    outfile << "additional_remarks:" + additional_stuff << std::endl;
     outfile.close();
 
     while(app) // Application still alive?
     {
         rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
 
-        rs2::frame depth = color_map(data.get_depth_frame()); // Find and colorize the depth data
+        // do i want to align it?
+        if (true)
+        {
+            //either one or the other
+            data = align_to_color.process(data);
+            //data = align_to_depth.process(data);
+        }
+        if (false)
+        {
+            //either one or the other
+            //data = align_to_color.process(data);
+            data = align_to_depth.process(data);
+        }
+
+
+        rs2::frame depth = data.get_depth_frame().apply_filter(color_map); // Find and colorize the depth data
         rs2::frame color = data.get_color_frame();            // Find the color data
 
               // Render depth on to the first half of the screen and color on to the second
@@ -82,8 +116,8 @@ int main(int argc, char * argv[]) try
         color_image.render(color, { app.width() / 2, 0, app.width() / 2, app.height() });
 
 
-        Mat color_p(Size(640, 480), CV_8UC3, (void*)color.get_data(), Mat::AUTO_STEP);
-        Mat color_d(Size(1280, 720), CV_8UC3, (void*)depth.get_data(), Mat::AUTO_STEP);
+        Mat color_p(Size(img_width, img_height), CV_8UC3, (void*)color.get_data(), Mat::AUTO_STEP);
+        Mat color_d(Size(img_width, img_height), CV_8UC3, (void*)depth.get_data(), Mat::AUTO_STEP);
 
         cv::cvtColor(color_p, color_p, CV_BGR2RGB);
 
